70ea330623a6d0a9aefc55b9a029d4a58e76219d
Web Dev Samrat <Abhishekkumar111127@gmail.com>
2022-03-10 20:39:16 +0530

    Update

--- script.js
+++ script.js
@@ -184,3 +184,25 @@ const observer = new IntersectionObserver(countingCounters, options);
 
 
 observer.observe(stats);
+
+/*=========================Testimonials Section========================*/
+
+const slides = document.querySelector(".slider").children;
+const indicatorImages = document.querySelector(".slider-indicator").children;
+
+for(let i=0; i < indicatorImages.length; i++){
+  indicatorImages[i].addEventListener('click', function(){
+
+    for(let j = 0; j < indicatorImages.length; j++){
+      indicatorImages[j].classList.remove("active");
+    }
+
+    this.classList.add('active');
+    const id = this.getAttribute('data-id');
+    for(let j = 0; j < slides.length; j++){
+      slides[j].classList.remove('active');
+    }
+
+    slides[id].classList.add('active');
+  })
+}
